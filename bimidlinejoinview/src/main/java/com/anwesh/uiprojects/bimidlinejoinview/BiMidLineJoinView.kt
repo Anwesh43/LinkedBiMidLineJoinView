a6fd7bbdd416ee79054c77960066ad668c0ccceb
Anwesh43 <anweshthecool0@gmail.com>
2020-09-25 01:07:04 +0530

    adding BiMidLineJoinView class

--- bimidlinejoinview/src/main/java/com/anwesh/uiprojects/bimidlinejoinview/BiMidLineJoinView.kt
+++ bimidlinejoinview/src/main/java/com/anwesh/uiprojects/bimidlinejoinview/BiMidLineJoinView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawBMLJNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiMidLineJoin(scale, w, h, paint)
 }
+
+class BiMidLineJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
